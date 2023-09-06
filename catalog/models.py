595fcdc32e6e9a4ac0713db4f3389b71193aef81
Natalia Melnik <nataliamelnik138@gmail.com>
2023-09-06 22:58:40 +0600

    Добавлено поле created_at в модель Category. Создана миграция

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(**NULLABLE, verbose_name='Изображение')
+    category = models.CharField(max_length=150, verbose_name='Наименование')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
+    date_create = models.DateField(verbose_name='Дата создания')
+    last_modified_date = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.product_name}'
+
+    class Meta:
+        verbose_name = 'продукт'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'продукты'  # Настройка для наименования набора объектов
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=150, verbose_name='Категория')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    created_at = models.CharField(max_length=50, default='nullexit')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории'  # Настройка для наименования набора объектов
