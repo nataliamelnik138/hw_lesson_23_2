c720560ed1003ad9228a96a7b19aaf7b03ea6819
Natalia Melnik <nataliamelnik138@gmail.com>
2023-09-08 21:56:00 +0600

    Добавлено отображение моделей в админку

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'category_name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product_name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description',)
