a57b499eaca65b928790bdfc7d697d559e514956
Natalia Melnik <nataliamelnik138@gmail.com>
2023-09-08 21:52:37 +0600

    Исправлена модель

--- catalog/models.py
+++ catalog/models.py
@@ -4,11 +4,26 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
+class Category(models.Model):
+    category_name = models.CharField(max_length=150, verbose_name='Категория')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    # created_at = models.DateField(verbose_name='Дата создания')
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.category_name}'
+
+    class Meta:
+        verbose_name = 'категория'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'категории'  # Настройка для наименования набора объектов
+
+
 class Product(models.Model):
     product_name = models.CharField(max_length=150, verbose_name='Наименование')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
     image = models.ImageField(**NULLABLE, verbose_name='Изображение')
-    category = models.CharField(max_length=150, verbose_name='Наименование')
+    # category = models.CharField(max_length=150, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория', )
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     date_create = models.DateField(verbose_name='Дата создания')
     last_modified_date = models.DateField(verbose_name='Дата последнего изменения')
@@ -22,15 +37,3 @@ class Product(models.Model):
         verbose_name_plural = 'продукты'  # Настройка для наименования набора объектов
 
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=150, verbose_name='Категория')
-    description = models.TextField(**NULLABLE, verbose_name='Описание')
-    created_at = models.CharField(max_length=50, default='nullexit')
-
-    def __str__(self):
-        # Строковое отображение объекта
-        return f'{self.category_name}'
-
-    class Meta:
-        verbose_name = 'категория'  # Настройка для наименования одного объекта
-        verbose_name_plural = 'категории'  # Настройка для наименования набора объектов
