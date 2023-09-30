c04fd413fddbf8949264bd56adb4e2b7691a99ba
Natalia Melnik <nataliamelnik138@gmail.com>
2023-09-30 19:30:25 +0600

    Добавлены контроллеры для входа, выхода, регистрации

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class LoginView(BaseLoginView):
+    template_name = 'users/login.html'
+
+
+class LogoutView(BaseLogoutView):
+    pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+    template_name = 'users/register.html'
+
+
