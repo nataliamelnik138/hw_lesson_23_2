1ba6bc0e9f0346d4e419ae260a3eef7713956791
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-01 11:33:45 +0600

    Описаны контроллеры регистрации, верификации и получения нового пароля

--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
+import random
+
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView, UpdateView
 
-from users.forms import UserRegisterForm
+from config import settings
+from users.forms import UserRegisterForm, UserForm
 from users.models import User
 
 
@@ -19,7 +23,65 @@ class LogoutView(BaseLogoutView):
 class RegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
-    success_url = reverse_lazy('users:login')
+    success_url = reverse_lazy('users:expectation')
     template_name = 'users/register.html'
 
+    def form_valid(self, form):
+        user = form.save()
+        email = user.email
+        pk = user.pk
+        user.is_active = False
+        user.save()
+        url = f'http://127.0.0.1:8000/users/verification/{pk}'
+        send_mail(
+            subject='Подтверждение регистрации',
+            message=f'Для подтверждения регистрации перейдите по ссылке {url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[email]
+        )
+        return super().form_valid(form)
+
+
+class VerificationView(TemplateView):
+    template_name = 'users/verification.html'
+    extra_context = {
+        'title': 'Поздравляем! Вы успешно зарегистрировались'
+    }
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        user = User.objects.get(pk=self.kwargs.get('pk'))
+        if user:
+            user.is_active = True
+            user.save()
+        return self.render_to_response(context)
+
+
+def expectation(request):
+    context = {
+        'title': 'Подтверждения регистрации'
+    }
+    return render(request, 'users/expectation.html', context)
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    success_url = reverse_lazy('users:profile')
+    form_class = UserForm
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(8)])
+    send_mail(
+        subject='Вы запросили новый пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('catalog:index'))
 
