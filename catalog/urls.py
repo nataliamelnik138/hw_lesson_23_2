c1166fd49c07a63c0ad7c0172618d93595d7ba24
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-01 13:28:43 +0600

    Закрыты для анонимных пользователей все контроллеры, которые отвечают за работу с продуктами

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from catalog.apps import CatalogConfig
 from catalog.views import contact, IndexView, ProductDetailView, ProductCreateView, ProductListView, ProductUpdateView, \
@@ -9,13 +10,13 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('contacts/', contact, name='contact'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
-    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/', login_required(ProductDetailView.as_view()), name='product'),
+    path('create/', login_required(ProductCreateView.as_view()), name='product_create'),
     path('list/', ProductListView.as_view(), name='list_product'),
-    path('edit_product/<int:pk>', ProductUpdateView.as_view(), name='edit_product'),
-    path('delete_product/<int:pk>', ProductDeleteView.as_view(), name='delete_product'),
-    path('product/<int:pk>/create_version/', VersionCreateView.as_view(), name='version_create'),
-    path('edit_version/<int:pk>', VersionUpdateView.as_view(), name='edit_version'),
-    path('delete_version/<int:pk>', VersionDeleteView.as_view(), name='delete_version'),
+    path('edit_product/<int:pk>', login_required(ProductUpdateView.as_view()), name='edit_product'),
+    path('delete_product/<int:pk>', login_required(ProductDeleteView.as_view()), name='delete_product'),
+    path('product/<int:pk>/create_version/', login_required(VersionCreateView.as_view()), name='version_create'),
+    path('edit_version/<int:pk>', login_required(VersionUpdateView.as_view()), name='edit_version'),
+    path('delete_version/<int:pk>', login_required(VersionDeleteView.as_view()), name='delete_version'),
 
 ]
