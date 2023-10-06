06a7d2edbb894c5fdb3d00515f9037223b964cc8
Natalia Melnik <nataliamelnik138@gmail.com>
2023-10-06 21:59:27 +0600

    Настроено кеширование

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
+
 from blog.apps import BlogConfig
 from blog.views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView
 
@@ -7,8 +9,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='list'),
-    path('create/', BlogPostCreateView.as_view(), name='create'),
-    path('edit/<slug>', BlogPostUpdateView.as_view(), name='edit'),
+    path('create/', never_cache(BlogPostCreateView.as_view()), name='create'),
+    path('edit/<slug>', never_cache(BlogPostUpdateView.as_view()), name='edit'),
     path('view/<slug>', BlogPostDetailView.as_view(), name='view'),
     path('delete/<slug>', BlogPostDeleteView.as_view(), name='delete'),
 ]
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_page, never_cache
 
 from catalog.apps import CatalogConfig
 from catalog.views import contact, IndexView, ProductDetailView, ProductCreateView, ProductListView, ProductUpdateView, \
-    ProductDeleteView, VersionCreateView, VersionUpdateView, VersionDeleteView
+    ProductDeleteView, VersionCreateView, VersionUpdateView, VersionDeleteView, categories
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('contacts/', contact, name='contact'),
-    path('product/<int:pk>/', login_required(ProductDetailView.as_view()), name='product'),
-    path('create/', login_required(ProductCreateView.as_view()), name='product_create'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product'),
+    path('create/', never_cache(ProductCreateView.as_view()), name='product_create'),
     path('list/', ProductListView.as_view(), name='list_product'),
-    path('edit_product/<int:pk>', login_required(ProductUpdateView.as_view()), name='edit_product'),
-    path('delete_product/<int:pk>', login_required(ProductDeleteView.as_view()), name='delete_product'),
-    path('product/<int:pk>/create_version/', login_required(VersionCreateView.as_view()), name='version_create'),
-    path('edit_version/<int:pk>', login_required(VersionUpdateView.as_view()), name='edit_version'),
-    path('delete_version/<int:pk>', login_required(VersionDeleteView.as_view()), name='delete_version'),
+    path('edit_product/<int:pk>', never_cache(ProductUpdateView.as_view()), name='edit_product'),
+    path('delete_product/<int:pk>', ProductDeleteView.as_view(), name='delete_product'),
+    path('product/<int:pk>/create_version/', VersionCreateView.as_view(), name='version_create'),
+    path('edit_version/<int:pk>', VersionUpdateView.as_view(), name='edit_version'),
+    path('delete_version/<int:pk>', VersionDeleteView.as_view(), name='delete_version'),
+    path('categories', categories, name='categories'),
 
 ]
